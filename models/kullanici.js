16515ab6fc2bbcf26374b4a2786171eef24f0f0d
Tunç Gürsoy <tuncgursoy@hotmail.com>
2020-08-28 20:56:12 +0300

    master

---
+++ models/kullanici.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Creates ||Kullanci|| Schema
+
+const kullanciSchema = new Schema({
+    _email : String,
+    _telefon : String,
+    _firma : String,
+    _isim : String, 
+    _soyisim : String,
+    _sifre : String 
+
+})
+module.exports = kullancilarModel = mongoose.model("kullanici",kullanciSchema,"kullanci")
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+var createError = require("http-errors");
+
+const app = express();
+
+/* MODEL IMPORTS */
+const kullanciModel = require("./models/kullanici");
+
+
+/* MIDDLEWARES */
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/* INIT DB */
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.error("DB connection error: ", err));
+
+
+
+/* ROUTES */
+ 
+app.get("/", (req, res) => {
+    res.send("root");
+});
+app.get("/kullanici", async (req, res, next) => {
+    try {
+        const requests = await requestsModel.find({}).select({ _id: 0 });
+        res.json(requests);
+        print(requests)
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+
+/* ERROR HANDLER */
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({ error: err.message });
+});
+
+/* 404 HANDLER */
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!");
+});
+
+/* SERVER RUNNER */
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
